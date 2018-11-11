bb3efaece58ec627922ca84a58668a222387af17
Alexander Moon <ajmoon2@wisc.edu>
2018-11-11 10:09:34 -0600

    Update typeclasses/characters.py

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -34,5 +34,5 @@ class Character(DefaultCharacter):
 
     def at_object_creation(self):
         self.cmdset.add("commands.custom_cmdsets.CharacterCmdSet", True)
-        self.contents.add(create_object("typeclasses.objects.Beer", "beer"))
+        self.contents.append(create_object("typeclasses.objects.Beer", "beer"))
     pass
